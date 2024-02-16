f611753f80284cea7b18a47c1e1f0e48afd37097
박유진 <lakeyoujin@korea.ac.kr>
2024-02-16 19:36:28 +0900

    도메인 연결

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['15.164.161.237']
+ALLOWED_HOSTS = ['15.164.161.237', 'rorschachtest.org']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
